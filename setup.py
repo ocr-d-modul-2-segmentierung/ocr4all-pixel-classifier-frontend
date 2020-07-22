574ac535af39452822ddfa9cec22240efbf28d0c
Alexander Gehrke <alexander.gehrke@uni-wuerzburg.de>
2020-07-22 15:35:20 +0200

    Update homepage

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     include_package_data=True,
     author="Christoph Wick, Alexander Hartelt, Alexander Gehrke",
     author_email="christoph.wick@informatik.uni-wuerzburg.de, alexander.hartelt@informatik.uni-wuerzburg.de, alexander.gehrke@informatik.uni-wuerzburg.de",
-    url="https://gitlab2.informatik.uni-wuerzburg.de/chw71yx/page-segmentation.git",
+    url="https://gitlab2.informatik.uni-wuerzburg.de/ocr4all-page-segmentation/ocr4all-pixel-classifier-frontend",
     entry_points={
         'console_scripts': [
             'ocr4all-pixel-classifier=ocr4all_pixel_classifier_frontend.main:main',
